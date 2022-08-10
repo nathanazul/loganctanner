1ee35b85afcc29b9504e281d5fac32b6a0a3a33b
nathanazul <33742124+nathanazul@users.noreply.github.com>
2022-08-10 15:56:12 -0400

    end day 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
+import { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {useState} from 'react';
+
+
+function Tit() {
+  return (
+    <h1 className='lct'>
+      LoganCTanner
+    </h1>
+  )
+};
+
+/*
+MENU
+description:
+a menu selector which determines the page being viewed
+
+TODO
+* on page load:
+** animation: fadeIn(loganCTanner), shrink && move to UL corner
+** animation: creator->creator polymath->creator polymath gamer
+** fade in offset
+** shrinks && move to UR corner
+** closeMenu()
+* defaults to creator
+* onClick(): show offset->expand down, revealing other options 
+** (onClick(outsideMenu) || noAction(4sec)) ? closeMenu() : ''
+
+
+*/
+function Menu() {
+  //const [isMenuExapnded, setIsMenuExpanded] = useState(false);
+  const pages = ["creator", "polymath", "gamer"];
+
+  const listPages = pages.map(
+    (page) => <li key={page} className={page}>{page}</li>
+  );
+
+  return(
+    <ul>{listPages}</ul>
+  )
+
+}
+
+class Main extends Component {
+
+  render() {
+    return (
+      <>
+      <Tit />
+      <Menu />
+      </>
+    );
+  }
+
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Main />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
